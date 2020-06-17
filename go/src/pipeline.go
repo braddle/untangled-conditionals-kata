a0bca002099173a295630460b235206db693f0ef
Mark Bradley <braddle@gmail.com>
2020-06-17 11:20:15 +0100

    Final Change - Reordering functions

--- go/src/pipeline.go
+++ go/src/pipeline.go
@@ -24,6 +24,29 @@ func (p *Pipeline) run(project Project) {
 	p.email("Deployment completed successfully")
 }
 
+func (p *Pipeline) runTest(project Project) error {
+	if p.noTests(project) {
+		p.log.info("No tests")
+		return nil
+	}
+
+	if p.testsFailed(project) {
+		return errors.New("Tests failed")
+	}
+
+	p.log.info("Tests passed")
+	return nil
+}
+
+func (p *Pipeline) deploy(project Project) error {
+	if p.deployFailed(project) {
+		return errors.New("Deployment failed")
+	}
+
+	p.log.info("Deployment successful")
+	return nil
+}
+
 func (p *Pipeline) handleError(err error) {
 	p.log.error(err.Error())
 	p.email(err.Error())
@@ -39,32 +62,8 @@ func (p *Pipeline) email(content string)  {
 	p.emailer.send(content)
 }
 
-func (p *Pipeline) deploy(project Project) error {
-	if p.deployFailed(project) {
-		return errors.New("Deployment failed")
-	}
-
-	p.log.info("Deployment successful")
-	return nil
-}
-
 func (p *Pipeline) deployFailed(project Project) bool {
 	return "failure" == project.deploy()
-}
-
-func (p *Pipeline) runTest(project Project) error {
-	if p.noTests(project) {
-		p.log.info("No tests")
-		return nil
-	}
-
-	if p.testsFailed(project) {
-		return errors.New("Tests failed")
-	}
-
-	p.log.info("Tests passed")
-	return nil
-
 }
 
 func (p *Pipeline) noTests(project Project) bool {
